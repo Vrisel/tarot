2d05370299d9b90d2368c0c09782bed55a74f531
Vrisel <vrisel.p@gmail.com>
2022-01-26 08:52:26 +0900

    FEATURE: TS 컴포넌트 테스트 차 생성

---
+++ pages/tarot.vue
@@ -0,0 +1,15 @@
+<template>
+  <main>{{ msg }}</main>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+  name: 'TheTarot',
+  data () {
+    return {
+      msg: '타로',
+    };
+  },
+});
+</script>
